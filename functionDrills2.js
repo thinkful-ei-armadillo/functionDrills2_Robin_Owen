799592c3bd5e08c7a4f5e943be7f9c8ffaa34129
Robin Khiv <robinkhiv@gmail.com>
2019-01-14 12:48:31 -0800

    to infinity and beyond

--- functionDrills2.js
+++ functionDrills2.js
@@ -2,3 +2,17 @@ function jediName(firstName, lastName){
     return `${lastName.substring(0,4)}${firstName.substring(0,2)}`;
 }
 
+function beyond(num){
+    if (isFinite(num) === false){
+        console.log('and beyond');
+    }
+    else if (isFinite(num) === true && num > 0){
+        console.log('to infinity');
+    }
+    else if(isFinite(num) === true && num < 0){
+        console.log('to negative infinity');
+    }
+    else if(num === 0){
+        console.log('Staying home');
+    }
+}
