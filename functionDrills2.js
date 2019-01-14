e14c2d722ba4109a7665235a62d6e029243dfbd3
Robin Khiv <robinkhiv@gmail.com>
2019-01-14 13:18:08 -0800

    decode this

--- functionDrills2.js
+++ functionDrills2.js
@@ -16,3 +16,30 @@ function beyond(num){
         console.log('Staying home');
     }
 }
+
+function decode(string){
+    if(string.charAt(0) === "a"){
+        return string.charAt(1);
+    }
+    else if(string.charAt(0) === "b"){
+        return string.charAt(2);
+    }
+    else if(string.charAt(0) === "c"){
+        return string.charAt(3);
+    }
+   else if(string.charAt(0) === "d"){
+    return string.charAt(4);
+   } 
+    else {
+        return " ";
+    }  
+}
+
+function decodeMessage(str){
+    const strArr = str.split(' ');
+    let message = "";
+    for(let i = 0; i < strArr.length; i++){
+        message += decode(strArr[i]);
+    }
+    return message;
+}
